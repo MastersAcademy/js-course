7ce786ac7ae02dee02dc38dd0a6284d34f905155
RhinoGrey <i.rhinogrey.i@gmail.com>
2016-11-12 11:58:32 +0200

    Tabs changed to spaces

--- homeworks/vlad.goncharenko_RhinoGrey/homework_2/main.js
+++ homeworks/vlad.goncharenko_RhinoGrey/homework_2/main.js
@@ -1,129 +1,129 @@
 (function () {
 
 
-	//----------------------------------------------1----------------------------------------------------//
+    //----------------------------------------------1----------------------------------------------------//
 
-	var randomNumbers = [];
-
-
-	//----------------------------------------------2----------------------------------------------------//
-
-	function returnRandom() {
-
-    	return (Math.random() * (100 - 1) + 1).toFixed(0); //Random numbers generator
-
-	}
+    var randomNumbers = [];
 
 
-	for (var i = 0; i < 100; i++) {
+    //----------------------------------------------2----------------------------------------------------//
 
-		randomNumbers.push(returnRandom()); //Random numbers pusher
-	}
+    function returnRandom() {
 
-	console.log(randomNumbers);
+        return (Math.random() * (100 - 1) + 1).toFixed(0); //Random numbers generator
 
-
-	//----------------------------------------------3----------------------------------------------------//
-
-	var arrayToFind = [3, 6, 0, -6, 12];
+    }
 
 
-	function findMinMax(arrayToFind, MinMax) {
+    for (var i = 0; i < 100; i++) {
 
-		var minMaxValue = 0;
+        randomNumbers.push(returnRandom()); //Random numbers pusher
+    }
 
-		if (MinMax == 'max') {
-
-			for (var index in arrayToFind) {
-
-				if (minMaxValue < arrayToFind[index]) {
-
-					minMaxValue = arrayToFind[index];
-				}
-			}
-
-		} else if (MinMax == 'min') {
-
-			for (var index in arrayToFind) {
-
-				if (minMaxValue > arrayToFind[index]) {
-
-					minMaxValue = arrayToFind[index];
-				}
-			}
-		}
-
-		return minMaxValue;
-
-	}
-
-	var resultMax = findMinMax(arrayToFind, 'max');
-
-	var resultMin = findMinMax(arrayToFind, 'min');
-
-	console.log(resultMax);
-	
-	console.log(resultMin);
+    console.log(randomNumbers);
 
 
-	//----------------------------------------------4----------------------------------------------------//
+    //----------------------------------------------3----------------------------------------------------//
 
-	function convertArrayToObj (array) { //Array to object convertor
-
-		var obj = {};
-
-		var alphabet = 'abcdefghijklmnopqrstuvwxyz';
-
-		var i = 1;
-
-		obj = {};
-
-		for (var index in randomNumbers) {
-
-			if (index < 26) {
-
-				key = alphabet.charAt(+index);
-
-				obj[key] = randomNumbers[index];
-
-			} else if (i < randomNumbers.length - 1) {
-
-				obj[' ' + i] = randomNumbers[index];
-
-				i++;
-
-			}
-		}
-
-	return obj;
-
-	}
-
-	console.log(convertArrayToObj(randomNumbers));
+    var arrayToFind = [3, 6, 0, -6, 12];
 
 
-	//----------------------------------------------5----------------------------------------------------//
+    function findMinMax(arrayToFind, MinMax) {
 
-	function filterNew (newData, oldData) { //Array comparator
+        var minMaxValue = 0;
 
-		for (var indexOld in oldData ) {
+        if (MinMax == 'max') {
 
-			for (var indexNew in newData) {
+            for (var index in arrayToFind) {
 
-				if (newData[indexNew] == oldData[indexOld]) {
+                if (minMaxValue < arrayToFind[index]) {
 
-					newData.splice(indexNew, 1);
+                    minMaxValue = arrayToFind[index];
+                }
+            }
 
-				}
+        } else if (MinMax == 'min') {
 
-			}
+            for (var index in arrayToFind) {
 
-		}
+                if (minMaxValue > arrayToFind[index]) {
+
+                    minMaxValue = arrayToFind[index];
+                }
+            }
+        }
+
+        return minMaxValue;
+
+    }
+
+    var resultMax = findMinMax(arrayToFind, 'max');
+
+    var resultMin = findMinMax(arrayToFind, 'min');
+
+    console.log(resultMax);
+    
+    console.log(resultMin);
 
 
-	}
+    //----------------------------------------------4----------------------------------------------------//
 
-	var newData = [2, 12, 22, 6, 32];
+    function convertArrayToObj (array) { //Array to object convertor
+
+        var obj = {};
+
+        var alphabet = 'abcdefghijklmnopqrstuvwxyz';
+
+        var i = 1;
+
+        obj = {};
+
+        for (var index in randomNumbers) {
+
+            if (index < 26) {
+
+                key = alphabet.charAt(+index);
+
+                obj[key] = randomNumbers[index];
+
+            } else if (i < randomNumbers.length - 1) {
+
+                obj[' ' + i] = randomNumbers[index];
+
+                i++;
+
+            }
+        }
+
+    return obj;
+
+    }
+
+    console.log(convertArrayToObj(randomNumbers));
+
+
+    //----------------------------------------------5----------------------------------------------------//
+
+    function filterNew (newData, oldData) { //Array comparator
+
+        for (var indexOld in oldData ) {
+
+            for (var indexNew in newData) {
+
+                if (newData[indexNew] == oldData[indexOld]) {
+
+                    newData.splice(indexNew, 1);
+
+                }
+
+            }
+
+        }
+
+
+    }
+
+    var newData = [2, 12, 22, 6, 32];
 
     var oldData = [12, 5, 7, 17, 22, 32];
 
