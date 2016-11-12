be6fa4755f80531adb69d8d5103a98c8263e538a
Acid-base <bart133@gmail.com>
2016-11-12 22:17:27 +0200

    fix mistakes

--- homeworks/vladimir.varavva_Acid-base/homework_2/js/main.js
+++ homeworks/vladimir.varavva_Acid-base/homework_2/js/main.js
@@ -39,11 +39,10 @@
 // 4
 
     function convertArrayToObj(arr) {
-      var key, obj = {};
-          
+      var key, 
+          obj = {};   
       for (var i = 0; i < arr.length; i++) {
-        i < 26 ? key = String.fromCharCode(i + 97)
-               : key = i - 25;
+        key = i < 26 ? String.fromCharCode(i + 97) : i - 25;
         obj[key] = arr[i];
       }
       return obj;
@@ -57,15 +56,26 @@
   function filterNew(arr1, arr2) {
     for (var i = 0; i < arr1.length; i++) {
       for (var n = 0; n < arr2.length; n++) {
-        if (arr1[i] === arr2[n]) arr1.splice(i, 1);
+        if (arr1[i] === arr2[n]) {
+          arr1.splice(i, 1);
+          i--;
+        }
       }
     }
   }
 
   var newData = [2, 12, 22, 6, 32],
-      oldData = [12, 5, 7, 17, 22, 32];
+      oldData = [12, 5, 7, 17, 22, 32],
+      
+      arr1 = [2, 2, 3, 3, 15 ],
+      arr2 = [2, 3, 10]; 
 
   filterNew(newData, oldData);
   console.log(newData); 
+  
+  filterNew(arr1, arr2);
+  console.log(arr1);
 
 })();
+
+
