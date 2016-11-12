7f1d65d6d1fbdbbe7c6a30d927dcb9c9b672165e
Bohdan Popovych <bohdan.popovych@masterofcode.com>
2016-11-12 17:09:31 +0200

    added task 4 and 5

--- homeworks/bohdan.popovych_Rootwar/homework_2/main.js
+++ homeworks/bohdan.popovych_Rootwar/homework_2/main.js
@@ -1,14 +1,15 @@
 (function () {
   "use strict";
 
-  var numbers = [];
+  var numbers = [],
+    newData = [2, 12, 22, 6, 32],
+    oldData = [12, 5, 7, 17, 22, 32];
 
   for (var i = 1; i <= 100; i++ ) {
     numbers.push(Math.floor(Math.random() * (100 - 1) + 1));
   }
 
   function findMinMax (arr, type) {
-
     arr.sort(function(a, b) {
       return a - b;
     });
@@ -28,7 +29,43 @@
     }
   }
 
+  function convertArrayToObj(array) {
+    var obj = {};
+
+    for( var i = 0; i < array.length; i++ ) {
+      if(i < 26) {
+        obj[String.fromCharCode(97 + i)] = array[i];
+
+      } else {
+        obj[(i - 25)] = array[i]
+      }
+    }
+
+    return obj
+  }
+
+  function filterNew(arr1, arr2) {
+    for( var i = 0; i < arr1.length; i++ ) {
+      for( var j = 0; j < arr2.length; j++ ) {
+        if( arr1[i] === arr2[j] ) {
+          arr1.splice(i, 1);
+        }
+      }
+    }
+  }
+
+  console.log(/-------/);
+  console.log(convertArrayToObj(oldData));
+
+  console.log(/-------/);
+
+  filterNew(newData, oldData);
+  console.log(newData);
+
+  console.log(/-------/);
+
   findMinMax(numbers, 'min');
-  findMinMax(numbers, 'max')
+  findMinMax(numbers, 'max');
+  console.log(/-------/);
 
 })();
