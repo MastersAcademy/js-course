da4d6449e34467d6a3f2d15b6bcf07e7993f0733
bubman <vlad.orientir@gmail.com>
2016-11-13 21:24:27 +0200

    hw complete

---
+++ homeworks/vlad.gevel_bubman/homework_2/script1.js
@@ -0,0 +1,118 @@
+// z1
+
+(function () {
+
+    array = [];
+
+    console.log(array);
+//z2
+
+function getRandomInt(min, max) {
+
+    return Math.floor(Math.random() * (max - min + 1)) + min
+}
+    for (var i = 0; i <= 100; i++) {
+
+        array.push(getRandomInt(1, 100));
+
+    };
+
+    console.log(array);
+
+}());
+
+//z3
+
+function findMinMax(array,extr) {
+
+    var max = Math.max.apply(null, array);
+
+    var min = Math.min.apply(null, array);
+
+    if (extr == "max") {
+
+        return max;
+
+    }else if (extr == "min") {
+
+        return min;
+
+    }else {
+
+        console.log("error")
+
+    };
+};
+
+(function () {
+
+    var resultMax = findMinMax(array, 'max');
+
+    var resultMin = findMinMax(array, 'min');
+
+    console.log("Макс: " + resultMax);
+
+    console.log("Мін: " + resultMin);
+
+}());
+
+//z4
+
+function convertArrayToObj (arr) {
+
+    var key,
+
+        lit = "abcdefghijklmnopqrstuvwxyz",
+
+        obj = {};
+
+    for (var i = 1; i <= arr.length; i++){
+
+        if (i <= 26){
+
+            key = lit.charAt(i-1);
+
+            obj[key]=arr[i];
+
+        }else if (i > 26) {
+
+            key = i-26;
+
+            obj[key]=arr[i];
+
+        }
+
+    }
+
+    return obj;
+
+}
+
+console.log(convertArrayToObj(array));
+
+//z5
+
+function filterNew(arr1, arr2){
+
+    for (var i = 0; i <= arr1.length;i++){
+
+        if (arr2.indexOf(arr1[i]) !== -1){
+
+            arr1.splice(i,1);
+
+            i = 0;
+
+        }
+
+    }
+
+}
+
+    var newData = [2, 12, 22, 6, 32];
+
+    var oldData = [12, 5, 7, 17, 22, 32];
+
+filterNew(newData, oldData);
+
+console.log(newData);
+
