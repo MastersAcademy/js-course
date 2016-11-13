910c0b148eb47b67e62b7a52510cf820eda3a9d7
Noli29 <noli.pogorelova@gmail.com>
2016-11-14 00:09:42 +0200

    task 1

--- homeworks/NatashaPavlenko_Noli29/homework_2/main.js
+++ homeworks/NatashaPavlenko_Noli29/homework_2/main.js
@@ -1,4 +1,24 @@
-( function (){
+(
+  function () {
+    //Task 1
+    var a = [];
 
-})
-();
+    console.log('Length array:' + a.length);
+
+
+    //Task 2
+
+  a.Math.random();
+
+    //Task 3
+
+
+
+
+    //Task 4
+
+
+
+    //Task 5
+  }
+)();
