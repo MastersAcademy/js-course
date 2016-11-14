072d8c4d4884aac193e28e00f82f9663e2a7c307
Alexey Efimenko <sle8882@gmail.com>
2016-11-14 15:05:06 +0200

    task 4 changed

--- homeworks/alexey.efimenko_AlexeyVE/homework_2/js/main.js
+++ homeworks/alexey.efimenko_AlexeyVE/homework_2/js/main.js
@@ -16,11 +16,11 @@
 			}
 		}
 		console.log('2)', arr, 'Array length: ' , arr.length)
-	}
+	};
 	randomNumber();
 	
 	//3
-	var myArr = [-1, -4, 4, 5, -60, 99];
+	var myArr = [-1,111, -4, 4,-80, 5, -60, 99];
 	var resultMax = findMinMax(myArr, 'max');
 	var resultMin = findMinMax(myArr, 'min');
 	
@@ -43,7 +43,7 @@
 			console.log("findMinMax error!!! option must be 'min' or 'max' ");
 			return false;
 		}
-	}
+	};
 	console.log('3)',myArr)
 	console.log('resultMin: ' + resultMin);
 	console.log('resultMax: ' + resultMax);
@@ -54,22 +54,22 @@
 		var alph="abcdefghijklmnopqrstuvwxyz";
 		var letters=[];
 		letters=alph.split('');
-		array.map(function(el,index){
-			if(letters.length > index){
-				obj[letters[index]]=el;
+		for(var i=0;i < array.length;i++){
+			if(i < letters.length){
+				obj[letters[i]]=array[i];
 			}
-			else if(index >=letters.length){
-				var prop=(index-letters.length)+1;
-				obj[prop]=el;
+			else if(i >= letters.length){
+				var prop=(i-letters.length)+1;
+				obj[prop]=array[i];
 			}
-		});
+		};
 	return obj;	
-	}
+	};
 	console.log('4)',convertArrayToObj(arr));
 	
 	//5
-	var newData = [1,2,3,4,5,6,7,8,9,55,55,55,55,55,55,999,99,99,9,4,5,6,7,8,4];
-	var oldData = [9,88,77,99,999,8,7,55,6,4,4,3,2,1,5,5,5];
+	var newData = [1,2,3,4,5,6,7,8,9,777,55,55,55,55,55,55,999,99,99,9,4,5,6,7,8,4];
+	var oldData = [9,777,88,77,99,999,8,7,55,777,777,777,6,4,4,3,2,1,5,5,5];
 	function filterNew(newData, oldData) {
 		for (var indNew = 0; indNew < newData.length; indNew++) {
 			for (var indOld = 0; indOld < oldData.length; indOld++) {
