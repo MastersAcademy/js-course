265e9daf47917e97cc11ef5dfd32495f18d2983c
Sergii5854 <bondarenko.sergii.i@gmail.com>
2016-11-14 11:38:13 +0200

    HomeWork2 S5854

--- homeworks/Sergii.Bondarenko_Sergii5854/HomeWork_2/main.js
+++ homeworks/Sergii.Bondarenko_Sergii5854/HomeWork_2/main.js
@@ -42,6 +42,7 @@ task_5
 
 	
 while(myArr.length < 100) {
+
     var randomNumber = Math.ceil(Math.random()*100)
     
     if(myArr.indexOf(randomNumber) > -1) continue;
@@ -60,45 +61,45 @@ task_3
 определяющую действие функции. Функция должна возвратить найденный элемент.
 */
 	
-	function findMinMax(arrayToFind, minMax){
-		
+function findMinMax(arrayToFind, minMax){
+	
 
-		var m = arrayToFind[0];
+	var m = arrayToFind[0];
+	
+	if(minMax == 'max'){
 		
-		if(minMax == 'max'){
+		for(i = 0; i < arrayToFind.length; i++){
 			
-			for(i = 0; i < arrayToFind.length; i++){
+			if(m < arrayToFind[i]){
 				
-				if(m < arrayToFind[i]){
-					
-					m = arrayToFind[i];
-					
-				}
+				m = arrayToFind[i];
+				
 			}
-			
 		}
 		
-		else if(minMax == 'min'){
-				
-			for(i = 0; i < arrayToFind.length; i++){
-				
-				if(m > arrayToFind[i]){
-					
-					m = arrayToFind[i];
-					
-				}
-			}
-			
-		}
-		
-		return m;
 	}
 	
-	var resultMax = findMinMax(myArr, "max");
-	var resultMin = findMinMax(myArr, "min");
+	else if(minMax == 'min'){
+			
+		for(i = 0; i < arrayToFind.length; i++){
+			
+			if(m > arrayToFind[i]){
+				
+				m = arrayToFind[i];
+				
+			}
+		}
+		
+	}
 	
-	console.log("Answer for task 3: " + "Max = " + resultMax);
-	console.log("Answer for task 3: " + "Min = " + resultMin);
+	return m;
+}
+
+var resultMax = findMinMax(myArr, "max");
+var resultMin = findMinMax(myArr, "min");
+
+console.log("Answer for task 3: " + "Max = " + resultMax);
+console.log("Answer for task 3: " + "Min = " + resultMin);
 /*
 	var arrayToFind = [3, 6, 0, -6, 12]; - використовувати даний масив для пошуку 'max' і  'min' значиння? чи наший начальний масив ? 
 	*/
@@ -112,36 +113,36 @@ task_4
 
 
 
-	function convertArrayToObj(arrToObj){
+function convertArrayToObj(arrToObj){
+	
+	var names = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+	var obj = {};
+	var j = 1;
+	
+	for(i = 0; i < names.length; i++){
 		
-		var names = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-		var obj = {};
-		var j = 1;
-		
-		for(i = 0; i < names.length; i++){
-			
-			obj[names[i]] = arrToObj;
-			
-		}
-		
-		if(i != arrToObj.length){
-			
-			while(i < arrToObj.length){
-				
-				obj[j] = arrToObj;
-				i++;
-				j++;
-			
-			}
-			
-		}
-		
-		return obj;
+		obj[names[i]] = arrToObj;
 		
 	}
 	
-	myArr = convertArrayToObj(myArr);
-	console.log(myArr);
+	if(i != arrToObj.length){
+		
+		while(i < arrToObj.length){
+			
+			obj[j] = arrToObj;
+			i++;
+			j++;
+		
+		}
+		
+	}
+	
+	return obj;
+	
+}
+
+myArr = convertArrayToObj(myArr);
+console.log(myArr);
 
 /*
 task_5
@@ -150,38 +151,38 @@ task_5
 массиве. Функция ничего не возвращает, а изменяет напрямую первый массив.
 */
 
-	var newData = [2, 12, 22, 6, 32];
-	var oldData = [12, 5, 7, 17, 22, 32];
+var newData = [2, 12, 22, 6, 32];
+var oldData = [12, 5, 7, 17, 22, 32];
+
+function filterNew(arr1, arr2){
 	
-	function filterNew(arr1, arr2){
-		
-		for(i = 0; i < arr1.length; i++)			
-			for(j = 0; j <= arr2.length; j++){
+	for(i = 0; i < arr1.length; i++)			
+		for(j = 0; j <= arr2.length; j++){
+			
+			if(arr1[i] === arr2[j]){
 				
-				if(arr1[i] === arr2[j]){
-					
-					arr1.splice(i, 1);
-					
-				}
+				arr1.splice(i, 1);
 				
 			}
-		
-	}
+			
+		}
 	
-	filterNew(newData, oldData);
-	console.log("Answer for task 5: " + newData);
+}
+
+filterNew(newData, oldData);
+console.log("Answer for task 5: " + newData);
 
 
 	// Проверь решение filterNew с таким примером
 
-	var arr1 = [2, 2, 3, 3, 15 ] ; 
-	var arr2 = [2, 3, 10]; 
-	
+var arr1 = [2, 2, 3, 3, 15 ] ; 
+var arr2 = [2, 3, 10]; 
 
 
-	filterNew(arr1, arr2);
-	console.log("Answer for task 5.1: " + arr1);
-	console.log(arr1);
 
-	
+filterNew(arr1, arr2);
+console.log("Answer for task 5.1: " + arr1);
+console.log(arr1);
+
+
 })();
