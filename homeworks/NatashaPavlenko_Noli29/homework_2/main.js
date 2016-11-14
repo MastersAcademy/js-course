1f08fd194231137c3143df30e42564e7e6b11b01
Noli29 <noli.pogorelova@gmail.com>
2016-11-14 20:58:32 +0200

    task 2, 3

--- homeworks/NatashaPavlenko_Noli29/homework_2/main.js
+++ homeworks/NatashaPavlenko_Noli29/homework_2/main.js
@@ -1,19 +1,39 @@
-(
-  function () {
+;(function () {
+
     //Task 1
-    var a = [];
-
-    console.log('Length array:' + a.length);
-
+    var arr = [];
 
     //Task 2
+    function getRandomArray(min, max) {
+        for (var i = 1; i <= max; i++) {
+            arr.push(Math.floor(Math.random() * (max - min) + min));
+        }
+        return arr;
+    }
+    var arrRandom = getRandomArray(1, 101);
+    console.log(arrRandom);
 
-  a.Math.random();
+
 
     //Task 3
+    function findMinMax (arr) {
+        for(i = 0; i < arr.length; i++) {
+            arr[i];
+        }
+        var max = Math.max.apply(null, arr);
+        return max;
 
+    }
 
+    var resultMax = findMinMax(arrayToFind, 'max');
 
+        var resultMin = findMinMax(arrayToFind, 'min');
+
+    console.log(resultMax); // 12
+
+    console.log(resultMin); // -6
+    var arrRandom = getRandomArray(1, 101);
+    console.log(arrRandom)
 
     //Task 4
 
