7794a061ff176512ec479421e4bc64ac9c5da30b
ucozlab <artem@ucoz-team.net>
2016-11-14 22:09:08 +0200

    fixed whitespaces

--- homeworks/artem.luzhanovskyi_ucozlab/homework_2/js/main.js
+++ homeworks/artem.luzhanovskyi_ucozlab/homework_2/js/main.js
@@ -1,82 +1,81 @@
-/**
- * Created by Admin on 12.11.2016.
- */
 (function () {
 
-	var array = []; // task 1
+  var array = []; // task 1
 
-	for (var i = 0; i < 100; i++) {
-		array.push(Math.round(Math.random() * 100)); // task 2
-	}
+  for (var i = 0; i < 100; i++) {
+    array.push(Math.round(Math.random() * 100)); // task 2
+  }
 
-	function findMinMax(arrayToFind, string) { // task 3
-		if (string == 'max') {
-			return Math.max.apply(Math, array);
-		} else if (string == 'min') {
-			return Math.min.apply(Math, array);
-		} else {
-			return array;
-		}
-	}
+  function findMinMax(arrayToFind, string) { // task 3
+    if (string == 'max') {
+      return Math.max.apply(Math, array);
+    } else if (string == 'min') {
+      return Math.min.apply(Math, array);
+    } else {
+      return array;
+    }
+  }
 
-	var resultMax = findMinMax(array, 'max');
-	var resultMin = findMinMax(array, 'min');
+  var resultMax = findMinMax(array, 'max');
 
-	console.log(resultMax);
-	console.log(resultMin);
+  var resultMin = findMinMax(array, 'min');
 
-	function convertArrayToObj(array) { // task 4
+  console.log(resultMax);
 
-		var obj = {},
-			j = 1;
+  console.log(resultMin);
 
-		for (var i = 0; i <= array.length - 1; i++) {
+  function convertArrayToObj(array) { // task 4
 
-			var key = String.fromCharCode(i + 97);
+    var obj = {},
+      j = 1;
 
-			if (97 + i >= 123) {
-				obj[j] = array[i]; // numbers
-				j++;
-			} else {
-				obj[key] = array[i]; // 'alphabet'
-			}
+    for (var i = 0; i <= array.length - 1; i++) {
 
-		}
+      var key = String.fromCharCode(i + 97);
 
-		return obj;
-	}
+      if (97 + i >= 123) {
+        obj[j] = array[i]; // numbers
+        j++;
+      } else {
+        obj[key] = array[i]; // 'alphabet'
+      }
 
-	var resultConvertArrayToObj = convertArrayToObj(array);
+    }
 
-	console.log(resultConvertArrayToObj);
+    return obj;
+  }
 
-	// task5
+  var resultConvertArrayToObj = convertArrayToObj(array);
+  console.log(resultConvertArrayToObj);
 
-	var newData = [2, 12, 22, 6, 32];
-	var oldData = [12, 5, 7, 17, 22, 32];
+  // task5
 
-	filterNew(newData, oldData);
+  var newData = [2, 12, 22, 6, 32];
 
-	function filterNew(array1, array2) {
-		for (var i = 0; i < array1.length; i++) {
-			array2.forEach(function (j) {
-				if (array1[i] === j) {
-					array1.splice(i, 1);
-					i--;
-				}
-			});
-		}
-	}
+  var oldData = [12, 5, 7, 17, 22, 32];
 
-	console.log(newData); // in console : [2, 6];
+  filterNew(newData, oldData);
 
-	// fixes
+  function filterNew(array1, array2) {
+    for (var i = 0; i < array1.length; i++) {
+      array2.forEach(function (j) {
+        if (array1[i] === j) {
+          array1.splice(i, 1);
+          i--;
+        }
+      });
+    }
+  }
 
-	var arr1 = [2, 2, 3, 3, 15];
-	var arr2 = [2, 3, 10];
+  console.log(newData); // in console : [2, 6];
 
-	filterNew(arr1, arr2);
+  // fixes
 
-	console.log(arr1);
+  var arr1 = [2, 2, 3, 3, 15];
+  var arr2 = [2, 3, 10];
+
+  filterNew(arr1, arr2);
+
+  console.log(arr1);
 
 })();
