ac73a7380c64f61b1336a94d4f8a4d1ca2ef17ca
Роман <roman.vertyporoh@gmail.com>
2016-11-14 19:41:40 +0200

    Update main.js

--- homeworks/Roman.Vertyporoh_Awake123/homework_1/Homework2/main.js
+++ homeworks/Roman.Vertyporoh_Awake123/homework_1/Homework2/main.js
@@ -3,85 +3,85 @@
 
 //2. Filled with 100 integer numbers. 
 
-     function addRandom(min, max) {
+  function addRandom(min, max) {
 
-          return Math.floor(Math.random() * (max - min) + min);
-     }
+       return Math.floor(Math.random() * (max - min) + min);
+  }
 
-          for(var i = 0; i < 100; i++) {
+       for(var i = 0; i < 100; i++) {
 
                arr.push(addRandom(1, 100));
           }
-     console.log(arr);
+  console.log(arr);
 
 //3. Function of searching min/max array element done.
 
-     var arrayToFind = [3, 6, 0, -6, 12];
-     var resultMax = findMinMax(arrayToFind, 'max');
-     var resultMin = findMinMax(arrayToFind, 'min');
+  var arrayToFind = [3, 6, 0, -6, 12];
+  var resultMax = findMinMax(arrayToFind, 'max');
+  var resultMin = findMinMax(arrayToFind, 'min');
 
-     function findMinMax(arrayToFind, str) {
+       function findMinMax(arrayToFind, str) {
 
-          var sortFunc = function(a, b) {
-               return a-b;
-          }
+            var sortFunc = function(a, b) {
+                 return a-b;
+       }
 
-          newArr = arrayToFind.sort(sortFunc);
+       newArr = arrayToFind.sort(sortFunc);
 
-          if(str == 'max'){
-               return newArr[newArr.length-1];
-          }
+            if(str == 'max'){
+                 return newArr[newArr.length-1];
+            }
 
-          else if(str == 'min'){
-               return newArr[0];
-          }
-     }
+            else if(str == 'min'){
+                 return newArr[0];
+            }
+       }
 
-     console.log(resultMax);
-     console.log(resultMin);
+  console.log(resultMax);
+  console.log(resultMin);
 
 //4. Convert array into object
 
-     function convertArrayToObj(array) {
+  function convertArrayToObj(array) {
 
-          var obj = {};
-          var alphabet = 'abcdefghijklmnopqrstuvwxyz';
-          var j = 1;
+       var obj = {};
+       var alphabet = 'abcdefghijklmnopqrstuvwxyz';
+       var j = 1;
 
-               for(var i = 0; i < array.length; i++) {
+            for(var i = 0; i < array.length; i++) {
 
-                    if (i < alphabet.length) {
+                 if (i < alphabet.length) {
                          obj[alphabet.charAt(i)] = array[i];
 
-          }
-                    else if(i > alphabet.length) {
+                 }
+                 else if(i > alphabet.length) {
                          obj[j++] = array[i];
-          }
-     }
+                 }
+            }
 
      return obj;
 
-}
-     console.log(convertArrayToObj(arr));
+  }
+  console.log(convertArrayToObj(arr));
 
 
 //5. Array filter function.
 
-     var newData = [2, 12, 22, 6, 32];
-     var oldData = [12, 5, 7, 17, 22, 32];
+  var newData = [2, 12, 22, 6, 32];
+  var oldData = [12, 5, 7, 17, 22, 32];
 
-     function filterNew(arr1, arr2){
+       function filterNew(arr1, arr2){
 
-          for(var i = 0; i <= arr1.length; i++){
+            for(var i = 0; i <= arr1.length; i++){
 
-              for(var j = 0; j < arr2.length; j++){
+                 for(var j = 0; j < arr2.length; j++){
 
-                   if (arr1[i] == arr2[j]){
-                        arr1.splice(i, 1);
-                    }
-               }
-          }
-     }
+                      if (arr1[i] == arr2[j]){
+                      arr1.splice(i, 1);
+                 }
+            }
+       }
+  }
 
-     filterNew(newData, oldData);
-     console.log(newData);
+  filterNew(newData, oldData);
+  console.log(newData);
