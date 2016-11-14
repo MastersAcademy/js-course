2a6233d877c17d772d840c80389fc601527b5fa5
gaik <djgaik85@gmail.com>
2016-11-15 01:30:56 +0200

    add hw2

---
+++ homeworks/gaik.grigoryan_DjGaik/homework_2/js/main.js
@@ -0,0 +1,18 @@
+(function () {
+	// 1
+	var arr = [];
+	console.log(arr);
+
+	// 2
+	for (var i = 1; i <= 100; i++) {
+		arr.push(Math.floor(Math.random() * 100) + 1);
+	}
+	console.log(arr);
+
+	// 3
+	
+
+	// 4
+
+	// 5
+})();
