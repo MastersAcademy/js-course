4e4c78e623f12bfb65c1234617517234c3ce9e73
Lord Senya <lord_senya@ukr.net>
2016-11-14 18:22:31 +0200

    fix Math.random() and tab

--- homeworks/sergey.belazerskiy_lordsenya/homework_2/main.js
+++ homeworks/sergey.belazerskiy_lordsenya/homework_2/main.js
@@ -2,71 +2,71 @@
 	var arr = [];
 	var newArray = [];
 	for (var i = 0; i < 100; i++) {
-		arr[i] = Math.random();
+		arr[i] = Math.floor(Math.random() * (100 - 1 + 1)) + 1;
 	}
-	
+	//console.log(arr);
+
 	var arrayToFind = [3, 6, 0, -6, 12];
 
-	var resultMax = findMinMax(arrayToFind, 'max');
+	var resultMax = findMinMax(arrayToFind, 'max'); // 12
 
-	var resultMin = findMinMax(arrayToFind, 'min');
+	var resultMin = findMinMax(arrayToFind, 'min');  // -6
 
 	function findMinMax(ourArray,minmax){
 		if (Array.isArray(ourArray) == true) {
 			var compare = ourArray[0];
-			ourArray.forEach(function(element,ourNewArray) {
-				if (minmax == "min") {
-					if (element < compare) {
-						compare = element;
-					}
-				}else{
-					if (minmax == "max") {
-						if (element > compare) {
+				ourArray.forEach(function(element,ourNewArray) {
+					if (minmax == "min") {
+						if (element < compare) {
 							compare = element;
 						}
-					}else{ 
+					}else{
+						if (minmax == "max") {
+							if (element > compare) {
+								compare = element;
+							}
+						}else{ 
 							console.log("incorrect second parametr");
 						}
 					}
-			});	
+				});
 			console.log(compare);
-		}else{ console.log("incorrect first parametr");
+		}else{
+			console.log("incorrect first parametr");
 		}
-	}
-	obj = createArrey();
-	
-	function createArrey()  {
-		var array = [];
-		
-		for (var i = 0; i <=100;i++) {
-			array[i] =i;
-		}
-			convertArrayToObj (array);
-	}
-	
-	function convertArrayToObj (array) {
+	}  
 
-    var obj = {};
-	var numb;
+	obj = createArrey();
+
+	function createArrey() {
+		var array = [];
+		for (var i = 0; i <=100;i++) {
+			array[i] = Math.floor(Math.random() * (100 - 1 + 1)) + 1;
+		}
+		convertArrayToObj(array);
+	}
+
+	function convertArrayToObj(array) {
+		var obj = {};
+		var numb;
 		for ( var i = 0; i < array.length; i++) {
 			if (i <= 25){
 				obj[String.fromCharCode(i + 97)] = array[i];
 			}else{
-					var numb = i - 25;
+				var numb = i - 25;
 				obj[numb.toString()] = array[i];
 			}
 		}
- 
-    return obj;
-
+		//console.log(obj);
+		return obj;
 	}
-	
+
 	var newData = [2, 12, 22, 6, 32];
 
-    var oldData = [12, 5, 7, 17, 22, 32];
+	var oldData = [12, 5, 7, 17, 22, 32];
 
 	filterNew(newData, oldData);
-	
+
 	function filterNew(newData,oldData) {
 		for (var i = 0; i < oldData.length; i++){
 			for (var j = newData.length - 1; j !== 0 ; j--) {
@@ -77,5 +77,5 @@
 		}
 	}
 
-    console.log(newData); // in console : [2, 6];
+	console.log(newData); // in console : [2, 6];
 })();
