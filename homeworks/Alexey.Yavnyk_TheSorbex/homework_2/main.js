2420948a04c6ba3e1146a719d2f75be5783434c8
Alexey Yavnyk <thesorbex@gmail.com>
2016-11-15 02:52:09 +0200

    homework_2

--- homeworks/Alexey.Yavnyk_TheSorbex/homework_2/main.js
+++ homeworks/Alexey.Yavnyk_TheSorbex/homework_2/main.js
@@ -6,7 +6,7 @@
     //2
 
     for (var i = 0; i < 100; i++) {
-	    arr[i] = Math.floor(Math.random()*100);
+	      arr[i] = Math.floor(Math.random()*100);
     }
 
     //3
@@ -15,25 +15,25 @@
 
     function findMinMax(array, value){
 
-       var min = array[0], max = array[0];
+        var min = array[0], max = array[0];
 
-       if (value == 'max') {
-   	    for (var i = 0; i < array.length; i++) {
-   		    if (max < array[i]) {
-   			    max = array[i];
-   		    }
-   	    }
+        if (value == 'max') {
+   	        for (var i = 0; i < array.length; i++) {
+   		          if (max < array[i]) {
+   			        max = array[i];
+   		          }
+   	         }
 
    	    return max;
-       } else if (value == 'min') {
-   	    for (var i = 0; i < array.length; i++) {
-   		    if (min > array[i]) {
-   			    min = array[i];
-   		    }
-   	    }
+        } else if (value == 'min') {
+   	        for (var i = 0; i < array.length; i++) {
+   		          if (min > array[i]) {
+   			            min = array[i];
+   		          }
+   	        }
 
    	    return min;
-   	}
+   	    }
     }
 
     var resultMax = findMinMax(arrayToFind, 'max');
@@ -45,23 +45,23 @@
     //4
 
     function convertArrayToObj(array){
-	    var obj = {};
-	    var startStr = 97, endStr = 122;
-	    var k = 0;
-	    var keyNumb = 1;
+	      var obj = {};
+	      var startStr = 97, endStr = 122;
+	      var k = 0;
+	      var keyNumb = 1;
 
-	    while(k < array.length){
-		    if (startStr <= endStr) {
-		    key = String.fromCharCode(startStr);
-		    obj[key] = array[k];
-		    startStr++;
-	       } else {
-		    obj[keyNumb] = array[k];
-		    keyNumb++;
-	    }
+	      while(k < array.length){
+		        if (startStr <= endStr) {
+		        key = String.fromCharCode(startStr);
+		        obj[key] = array[k];
+		        startStr++;
+	          } else {
+		        obj[keyNumb] = array[k];
+		        keyNumb++;
+	      }
 
-	    k++;
-    }
+	      k++;
+        }
 
     return obj;
     }
@@ -74,14 +74,14 @@
     var oldData = [12, 5, 7, 17, 22, 32];
 
     function filterNew(array1, array2){
-       for (var i = 0; i < array2.length; i++) {
-          for (var j = 0; j < array1.length; j++) {
-      	    if (array2[i] == array1[j]) {
-      		    array1.splice(j,1);
-      		    j--;
-      	    }
-          }
-       }
+        for (var i = 0; i < array2.length; i++) {
+            for (var j = 0; j < array1.length; j++) {
+      	        if (array2[i] == array1[j]) {
+      		          array1.splice(j,1);
+      		          j--;
+      	        }
+            }
+        }
     }
 
     filterNew(newData, oldData);
