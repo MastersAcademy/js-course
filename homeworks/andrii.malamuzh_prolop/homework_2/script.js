77264137f686cd9fa6cc554c7668b18008e7e5cb
prolop <andrey.malamuzh.1994@gmail.com>
2016-11-15 09:33:10 +0200

    fixed

--- homeworks/andrii.malamuzh_prolop/homework_2/script.js
+++ homeworks/andrii.malamuzh_prolop/homework_2/script.js
@@ -44,7 +44,7 @@
         for (var i = 0; i < arr1.length; i++) {
             for (var j = 0; j < arr2.length; j++) {
                 if (arr1[i] == arr2[j]) {
-                    arr1.splice(i , 1);
+                    arr1.splice(i, 1);
                     i--;
                 }
             }
