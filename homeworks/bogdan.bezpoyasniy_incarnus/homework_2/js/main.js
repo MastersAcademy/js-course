9b9d0b18527f21f4e302d97c1046c3802db9e73a
incarnus <it.oipopp@ukr.net>
2016-11-15 12:01:09 +0200

    homework_2_2

--- homeworks/bogdan.bezpoyasniy_incarnus/homework_2/js/main.js
+++ homeworks/bogdan.bezpoyasniy_incarnus/homework_2/js/main.js
@@ -48,7 +48,7 @@ function convertArrayToObj(array) {
             if (i < 1) {
                 obj[String.fromCharCode(97 + n)] = array[n];
             } else {
-                obj[' '+i] = array[n];
+                obj[i] = array[n];
             }
 
         }
@@ -62,19 +62,29 @@ console.log(convertArrayToObj(numbers));
 console.log('--------- 5 ---------')
 
 //task 5
-var newData = [2, 12, 22, 6, 32];
-var oldData = [12, 5, 7, 17, 22, 32];
-    function filterNew(array1, array2) {
-        for (var i = 0; i < array1.length; i++) {
-            for (var n = 0; n < array2.length; n++) {
-                if (array1[i] == array2[n]) {
-                    array1.splice(i, 1);
-                }
+function filterNew(arr1, arr2) {
+
+        for(var i = 0; i<arr1.length; i++){
+
+            for(var j=0; j<arr2.length; j++){
+
+                    if(arr1[i]==arr2[j]) {
+
+                        arr1.splice(i, 1)
+
+                        i--;
+
+                    }
             }
         }
+
     }
+    var newData = [1, 1, 2, 2, 3];
+
+    var oldData = [1, 2, 5];
+
     filterNew(newData, oldData);
 
-console.log(newData);
+    console.log(newData); 
 
 })();
