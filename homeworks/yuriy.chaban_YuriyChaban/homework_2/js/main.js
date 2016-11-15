5e011a5bedb105695fbec437b522ff00f649fb94
Yuriy Chaban <shuterrush@gmail.com>
2016-11-15 09:16:18 +0200

    Update main.js

--- homeworks/yuriy.chaban_YuriyChaban/homework_2/js/main.js
+++ homeworks/yuriy.chaban_YuriyChaban/homework_2/js/main.js
@@ -49,14 +49,52 @@
         function findMinMax(arrayToFind,randomNumber) {
 
             var zero = 0;
+(function () {
 
-            if (randomNumber == "maxValue") {
-                var maxValue = Math.max.apply(zero, arrayToFind);
-                return maxValue;
+    // --------------------------Task 1---------------------
+    // -----------------------------------------------------
+        // created an empty array
+        console.log("----------------Task 1----------------");
+        console.log("Created an empty array");
+        console.log("--------------------------------------");
+        var numberArr = [];
+    // -----------------------------------------------------
+
+    // --------------------------Task 2---------------------
+    // -----------------------------------------------------
+        // create function randomNumberArray for random number from 0 to 100 items
+        function randomNumberArray(min, max) {
+            return Math.floor(Math.random() * (max - min)) + min;
+        };
+
+        // Pushing to numberArr items from 0 to 100
+        for (var i = 0; i < 100; i++) {
+            numberArr.push(i);
+            randomNumberArray(numberArr);
+        };
+        // Output numberArr
+        console.log("----------------Task 2----------------");
+        console.log("Output numberArr: ");
+        console.log(numberArr);
+        console.log("--------------------------------------");
+    // -----------------------------------------------------
+    
+    // --------------------------Task 3---------------------
+    // -----------------------------------------------------
+        // create arrayToFind
+        var arrayToFind = [3, 6, 0, -6, 12];
+
+        function findMinMax(arrayToFind,randomNumber) {
+
+            var zero = 0;
+
+            if (randomNumber == "max") {
+                var max = Math.max.apply(zero, arrayToFind);
+                return max;
             }
-            else if (randomNumber == "minValue") {
-                var minValue = Math.min.apply(zero, arrayToFind);
-                return minValue;
+            else if (randomNumber == "min") {
+                var min = Math.min.apply(zero, arrayToFind);
+                return min;
             }
             else {
                 console.log("Can`t determine the value!")
@@ -64,9 +102,9 @@
         };
 
         (function () {
-            var resultMax = findMinMax(arrayToFind, 'maxValue');
-            var resultMin = findMinMax(arrayToFind, 'minValue');
-             console.log("----------------Task 3----------------");
+            var resultMax = findMinMax(arrayToFind, 'max');
+            var resultMin = findMinMax(arrayToFind, 'min');
+            console.log("----------------Task 3----------------");
             console.log("Maximum: " + resultMax); 
             console.log("Mininmum: " + resultMin); 
             console.log("--------------------------------------");
@@ -77,17 +115,18 @@
     // -----------------------------------------------------
     // create convertArrayToObj 
         function convertArrayToObj (array) {
-            var keyForLetters,
-            strigLettears="abcdefghijklmnopqrstuvwxyz",
+            var key,
+            str="abcdefghijklmnopqrstuvwxyz",
             obj = {};
 
-            for (var i = 1; i <= array.length; i++){
-                if (i <= 26) {
-                    keyForLetters = strigLettears.charAt(i-1);
-                    obj[keyForLetters]=array[i];
-                }else if (i > 26) {
-                    keyForLetters = i-26;
-                    obj[" "+keyForLetters]=array[i];
+            for (var i = 0; i < array.length; i++) {
+                if (i < str.length){
+                    key = str.charAt(i);
+                    obj[key]=array[i];
+                }
+                else if (i >= str.length) {
+                    key = i-(str.length-1);
+                    obj[key]=array[i];
                 }
             }
             return obj;
@@ -101,12 +140,14 @@
     // --------------------------Task 5---------------------
     // -----------------------------------------------------
         // filter array function
+
+       // filter array function
         var newData = [2, 12, 22, 6, 32];
-        var oldData = [12, 5, 7, 17, 22, 32];	
+        var oldData = [12, 5, 7, 17, 22, 32];   
          function filterNew(arr1, arr2) {
              for (var i = 0; i < arr1.length; i++) {
                  for (var j = 0; j < arr2.length; j++) {
-                     if (arr1[i] == arr2[j]) {
+                     if (arr2.indexOf(arr1[i]) !== -1) {
                          arr1.splice(i, 1);
                      }
                  }
