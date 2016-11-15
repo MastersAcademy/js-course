bb5cfd938297d47dae7da5ddd88007efb3677b44
L-Sobchenko <sobchenko_luda@ukr.net>
2016-11-15 11:28:09 +0200

    added task 2 and 3

--- homeworks/Liudmila.Sobchenko_L-Sobchenko/homework_2/js/main.js
+++ homeworks/Liudmila.Sobchenko_L-Sobchenko/homework_2/js/main.js
@@ -1,4 +1,5 @@
 (function () {
+
     function createRandomArray (numberOfElements)
     {
         var array = [];
@@ -8,8 +9,49 @@
         return array;
     }
 
-    var arrayNatural = createRandomArray(100);
+    function findMinMax(arrayToFind, param){
+        var i = 0;
+        var mElement = arrayToFind[i];
+        var k = 1;
+        if (param == 'min') {k = -1};
+        while (i < arrayToFind.length) {
+            if (0 > (k * ((mElement - arrayToFind[i])))){
+                mElement = arrayToFind[i];
+            }
+            i++;
+        }
+        return mElement;
+    };
+
+    function convertArrayToObj (array) {
+
+        var obj = {};
+
+        /* Staff to convert array to obj
+         //...
+         */
+
+        return obj;
+
+    }
+
+    // task 1
+    var arrayNatural = [];
+
+    // task 2
+    arrayNatural = createRandomArray(100);
 
     console.log(arrayNatural);
+
+    // task 3
+    var arrayToFind = [3, 6, 0, -6, 12];
+
+    var resultMax = findMinMax(arrayToFind, 'max');
+
+    var resultMin = findMinMax(arrayToFind, 'min');
+
+    console.log(resultMax); // 12
+
+    console.log(resultMin); // -6
 
 })();
