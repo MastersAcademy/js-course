0662d2baa6ddb1c2f388acaa03b88605e734e495
Max Musiienko <maksonoff@gmail.com>
2016-11-15 11:32:32 +0200

    Homework_2 thrid try

--- homeworks/Maksym.Musiienko_Sempl/homework_2/js/main.js
+++ homeworks/Maksym.Musiienko_Sempl/homework_2/js/main.js
@@ -1,12 +1,12 @@
 (function () {
-
-	var arr1 = [];
-	for(var i = 0; i < 100; i++){
-		n = 0;
-		n = Math.floor(Math.random() * 100) + 1;
-
-            arr1.push(n);
-
+    
+    var arr1 = [];
+	  
+    for(var i = 0; i < 100; i++){
+	      
+        n = 0;
+	      n = Math.floor(Math.random() * 100) + 1;
+        arr1.push(n);
     }
     
     console.log(arr1);
@@ -21,35 +21,36 @@
     	
         var max = array.sort(function(a, b){return a - b});
 
-    	if (element == 'max') {
+    	  if (element == 'max') {
     		
-    	   return max[array.length - 1];
-    	}
-    	if (element == 'min') {
-    		return max[0];
-    	}
+    	      return max[array.length - 1];
+    	  }
+    	  if (element == 'min') {
+    		    
+            return max[0];
+    	  }
     }
   
-
-
-   console.log(resultMax);
-   console.log(resultMin);
+    console.log(resultMax);
+    console.log(resultMin);
    
-   console.log('**************task4*********************');
+    console.log('**************task4*********************');
 
-  function convertArrayToObj (array) {
+    function convertArrayToObj (array) {
 
-     var abc = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+        var abc = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
         var obj = {};
         var m = 1;
 
         for (var i = 0; i < array.length; i++) {
 
-            if (i > abc.length) {
+            if (i >= abc.length) {
+                
                 obj[m++] = array[i];
             }
 
-            else if (i <= abc.length) {
+            else {
+                
                 obj[abc[i]] = array[i];
             }
         }
@@ -60,27 +61,32 @@
     
     console.log('************task5************************');
       
-      var newData = [2, 12, 22, 6, 32];
+    var newData = [2, 12, 22, 6, 32];
 
-      var oldData = [12, 5, 7, 17, 22, 32];
+    var oldData = [12, 5, 7, 17, 22, 32];
      
 
-      function filterNew(array1, array2 ){
-      	for (i=0; i<array1.length; i++){
-      		for (j=0; j<array2.length; j++){
-      			if (array1[i] == array2[j])  {
-      			  array1.splice (i,1);
-      				i--;
-      			};
+    function filterNew(array1, array2 ){
+        
+        for (i=0; i<array1.length; i++){
+      	    
+            for (j=0; j<array2.length; j++){
+      			    
+                if (array1[i] == array2[j])  {
+                    
+                    array1.splice (i,1);
+      				      i--;
 
-      		}
+      			    };
+
+      		  }
       		
       	}
 
-      }
+    }
 
-      filterNew(newData, oldData);
+    filterNew(newData, oldData);
 
-      console.log(newData);
+    console.log(newData);
 
 })();
