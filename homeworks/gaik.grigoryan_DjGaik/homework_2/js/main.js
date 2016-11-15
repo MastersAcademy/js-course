dad54bdf19c43f6bb5d17ed9359ee9f521e0ab75
gaik <djgaik85@gmail.com>
2016-11-15 11:25:57 +0200

    hw2.3

--- homeworks/gaik.grigoryan_DjGaik/homework_2/js/main.js
+++ homeworks/gaik.grigoryan_DjGaik/homework_2/js/main.js
@@ -10,8 +10,34 @@
 	console.log(arr);
 
 	// 3
-	
+	var arrayToFind = [3, 6, 0, -6, 12];
+	var resultMax = function findMinMax(arrayToFind) {
+		var max = 0;
+		if (!(arrayToFind instanceof Array)) {
+			console.warn(arrayToFind, 'is not Array');
+			return;
+		}
+		arrayToFind.map(function (curent, i, array) {
+			// console.log(arguments);
+			max = Math.max(curent, max);
+		});
+		return(max);
+	}
+	console.log(resultMax(arrayToFind));
 
+	var resultMin = function findMinMax(arrayToFind) {
+		var min = 0;
+		if (!(arrayToFind instanceof Array)) {
+			console.warn(arrayToFind, 'is not Array');
+			return;
+		}
+		arrayToFind.map(function (curent, i, array) {
+			// console.log(arguments);
+			min = Math.min(curent, min);
+		});
+		return(min);
+	}
+	console.log(resultMin(arrayToFind));
 	// 4
 
 	// 5
