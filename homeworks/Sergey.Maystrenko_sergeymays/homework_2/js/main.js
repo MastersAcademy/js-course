493950559745621a6b1714ae5fb976423c3cfa27
sergeymays <maystrenkoworkmail@gmail.com>
2016-11-15 10:31:48 +0200

    homework_2

--- homeworks/Sergey.Maystrenko_sergeymays/homework_2/js/main.js
+++ homeworks/Sergey.Maystrenko_sergeymays/homework_2/js/main.js
@@ -7,11 +7,9 @@ console.log(myArray);
 
 console.log("****************2. Fill array with Math.random()*****************");
 
-
 for (var i = 0; i <= 99; i++) {
     myArray.push(Math.floor(Math.random() * 100 + 1));
 }
-
 console.log(myArray);
 
 console.log("*******************3. Finding min/max********************");
@@ -23,43 +21,30 @@ function findMinMax(arrayName, searchValue){
         return Math.min.apply(Math, arrayName);
     }
 }
-
 var resultMax = findMinMax(myArray, 'max');
-
 var resultMin = findMinMax(myArray, 'min');
-
 console.log(resultMax);
-
 console.log(resultMin);
 
 console.log("****************4. Convert array to object*****************");
 
-
-
 function convertArrayToObj (array) {
-var alphabetArray = [];
-
-for (var i = 97; i <= 122; i++){
-    alphabetArray.push(String.fromCharCode(i));
-}
-
-for (var i = 1; i <= 74; i++){
-    alphabetArray.push(i);
-}
+    var alphabetArray = [];
     var obj = {};
-
-    for (var i = 0; i < array.length; i++) {
-        obj[alphabetArray[i]] = array[i];
+    for (var i = 97; i <= 122; i++){
+        alphabetArray.push(String.fromCharCode(i));
     }
-
-    return obj;
-
-}
-
+    for (var i = 1; i <= array.length; i++){
+        alphabetArray.push(i);
+    }
+        for (var i = 0; i < array.length; i++) {
+            obj[alphabetArray[i]] = array[i];
+        }
+        return obj;
+    }
 console.log(convertArrayToObj(myArray));
 
 console.log("****************5. Array filter*****************");
-
 
 function filterNew(arr1, arr2){	
     for (var i = 0; i < arr1.length; i++){
@@ -73,11 +58,8 @@ function filterNew(arr1, arr2){
 
 }
 var newData = [1, 2, 3, 3, 4, 5];
-
 var oldData = [1, 3, 5];
-
 filterNew(newData, oldData);
-
 console.log(newData);
 
 })();
