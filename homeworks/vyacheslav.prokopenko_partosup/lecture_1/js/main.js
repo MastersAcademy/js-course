1c1a6a14a45e27f85dce571ddfd400ee4e964554
partosup <partosup@gmail.com>
2016-11-15 15:13:04 +0200

    #385 Lecture 1
    #385 errors fix

--- homeworks/vyacheslav.prokopenko_partosup/lecture_1/js/main.js
+++ homeworks/vyacheslav.prokopenko_partosup/lecture_1/js/main.js
@@ -1,79 +1,101 @@
 (function(){
 	
 	var arrayToFind = []; //new empty array
-	var resultMax,resultMin,newObj; //vars
-
 	
+	var resultMax,
+	
+		resultMin,
+		
+		newObj; //vars
 	
 	// push to array 100 items
 	for(var i = 0; i < 100; i++){ 
+	
 		arrayToFind.push(Math.floor(Math.random() * (100 - 1)) + 1);
+		
 	}
 	
 	console.log(arrayToFind);
 	
 	//init max and min value in array
-	function findMinMax(arr, param){           
+	function findMinMax(arr, param){   
+	        
 		var paramNum;
+		
 		var key = [];
 		
 		if(param == 'max'){						//to max value
+		
 			arr.forEach(function(item, i) {
+				
 		  		if(paramNum < item || i == 0){
+		  			
 					paramNum = item;
+					
 					key.push(i);
+					
 				}
+				
 			});
-			return 'max value: '+paramNum+' in keys ['+key+']';
+			
+			return paramNum;
 			
 		}else if(param == 'min'){				//to min value
+		
 			arr.forEach(function(item, i) {
+				
 		  		if(paramNum > item || i == 0){
+		  			
 					paramNum = item;
+					
 					key.push(i);
+					
 				}
+				
 			});
-			return 'min value: '+paramNum+' in keys ['+key+']';
+			
+			return paramNum;
 			
 		}else{									//undefined value
+		
 			console.log('undefined parametr in function findMinMax()');
+			
 		}
-    }
-    
+		
+    }    
     
 	resultMax = findMinMax(arrayToFind, 'max');
+	
    	resultMin = findMinMax(arrayToFind, 'min');
 
 	console.log(resultMax); 
+	
     console.log(resultMin); 
 	
 	//array to Object fun
 	function convertArrayToObj (arr) {
-		'use strict';
-	    var obj = {},alphaNum = [], keysLikeNumbers = 1;
+
+	    var obj = {},
+	    	keysLikeNumbers = 1;
 		
-		//generate alphabetic
-		function* generateAlphaNum() {
-	      // yield* generateSequence(97, 122);
-	      for (let i = 97; i <= 122; i++) yield i;
-	    }
-	    for(let code of generateAlphaNum()) {
-	      alphaNum.push(String.fromCharCode(code));
-	    }
-		
-		var lenghtOfalphaNum = alphaNum.length;
-		
+		var alphaNum = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+				
 		//convert array to Object
 		arr.forEach(function(item, i) {
-			if(lenghtOfalphaNum-1 > i || lenghtOfalphaNum-1 == i){
+			
+			if(alphaNum.length - 1 > i || alphaNum.length - 1 == i){
+				
 				obj[alphaNum[i]]= item; // keys a,b,c.....
+				
 			}else{
+				
 				obj[keysLikeNumbers]= item; // keys 1,2,3....
+				
 				keysLikeNumbers++;
+				
 			}
 	
 		});
-		
 		
 	    return obj;
 
@@ -83,29 +105,33 @@
 	
 	console.log(newObj);
 	
-
-	
 	//filter 2 arrays
-	function filterNew(arr1, arr2){
-		var lengthNewData = arr1.length;
-		var oldData = arr2.length;
-		
-		for( var i = 0; i < lengthNewData; i++){
-			for(var j = 0; j < oldData; j++){
-				if(arr1[i] == arr2[j]){
-					arr1.splice(i,1);
-				}
-			}
-		}
-		
+	function filterNew(arr1, arr2){	
+	
+	    for (var i = 0; i < arr1.length; i++){
+	    	
+	        for (var j = 0; j < arr2.length; j++){
+	        	
+	            if (arr1[i] == arr2[j]){
+	            	
+	                arr1.splice(i, 1);
+	                
+	                i--;
+	                
+	            }
+	            
+	        }
+	        
+	    }
+	    
 	}
 	
-	
-	
 	var newData = [2, 12, 22, 6, 32];
+	
     var oldData = [12, 5, 7, 17, 22, 32];
+    
     filterNew(newData, oldData);
+    
     console.log(newData); // in console : [2, 6];
 	
-
 })();
