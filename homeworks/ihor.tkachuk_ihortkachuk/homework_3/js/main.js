d3524854b4757fe33225f4c3f998515de83591d1
Ihor Tkachuk <ihortkachuk@yandex.ua>
2016-11-18 10:50:43 +0200

    new ver setTimeout fn

--- homeworks/ihor.tkachuk_ihortkachuk/homework_3/js/main.js
+++ homeworks/ihor.tkachuk_ihortkachuk/homework_3/js/main.js
@@ -1,12 +1,70 @@
 (function () {
-    window.oldSetTimeout = window.setTimeout;
 
-    window.setTimeout = function (delay, callback) {
-        return window.oldSetTimeout(callback, delay);
+    /**
+     * Overwrite function setTimeout
+     *
+     * @param setTimeout
+     * @returns {Function}
+     */
+    function mуTimeout(setTimeout) {
+        return function (delay, callback) {
+            return setTimeout(callback, delay);
+        };
+    }
+
+    window.setTimeout = mуTimeout(setTimeout);
+
+    setTimeout(1000, function () {
+        console.log(11);
+    });
+
+    /**
+     * Overwrite function setInterval
+     *
+     * @param callback
+     * @param delay
+     */
+    window.setInterval = function (callback, delay) {
+
     };
 
-    setTimeout(2000, function () {
-        console.log('testing');
-    })
+    function fncToDelay(param) {
+        console.log('Delayed run : ' + param);
+    }
+
+    /**
+     *
+     * @param delay
+     * @param fnc
+     * @returns {Function}
+     */
+    function freeze(delay, fnc) {
+        var timeout;
+
+        return function () {
+            var args = arguments;
+
+            if (timeout) {
+                clearTimeout(timeout);
+            }
+
+            timeout = setTimeout(delay, function () {
+                fnc.apply(this, args);
+            });
+        }
+    }
+
+    var frozenFunc = freeze(1000, fncToDelay);
+
+    frozenFunc('1');
+    frozenFunc('2');
+    frozenFunc('3');
+    frozenFunc('4');
+    frozenFunc('5');
+    frozenFunc('6');
+    frozenFunc('7');
+    frozenFunc('8');
+    frozenFunc('9');
+
 
 })();
