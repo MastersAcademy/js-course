5abe93bc6b3eb05f505f45dc80d00f6f6a1f1242
Acid-base <bart133@gmail.com>
2016-11-18 07:53:33 +0200

    small changes

--- homeworks/vladimir.varavva_Acid-base/homework_3/js/main.js
+++ homeworks/vladimir.varavva_Acid-base/homework_3/js/main.js
@@ -3,43 +3,24 @@
 
     //1
     var nativeSetTimeOut = window.setTimeout;
-//    var setTimeout;
 
-//
     window.setTimeout = function (delay, callback) {
         var args = Array.prototype.slice.call(arguments, 2);
-
-//        return nativeSetTimeOut(callback, delay, args);
         if (typeof callback === 'function') {
-
-        return nativeSetTimeOut(function() {
-//            var args = arguments;
-            var self = this;
-             callback = callback.apply(self, args);
-            callback;
-        }, delay);
+             nativeSetTimeOut(function () {
+                callback.apply(this, args);
+            }, delay);
         }
     };
 
-    function add(a, b, c) {
-        return console.log('hello, ' + a + ' ' + b + ' ' + c);
-    }
-
-        setTimeout(500, add, 'a', 45, null, NaN);
-
-
-
     //2
     window.setInterval = function (callback, delay) {
-
         var args = Array.prototype.slice.call(arguments, 2),
             timeout;
 
         (function loop() {
-            var result = callback.apply(this, args);
-            if (result !== false) {
-                timeout = setTimeout(delay, loop);
-            }
+            callback.apply(this, args);
+            timeout = setTimeout(delay, loop);
         }());
 
         return function () {
@@ -49,7 +30,5 @@
             }
         };
     };
-
-//                setInterval(add, 1500, 42, 45, 45, 45);
 
 })();
