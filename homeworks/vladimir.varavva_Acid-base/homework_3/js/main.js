a359263cbdf002fe6fc52cb37fd85432066a228b
Acid-base <bart133@gmail.com>
2016-11-19 13:15:28 +0200

    small changes

--- homeworks/vladimir.varavva_Acid-base/homework_3/js/main.js
+++ homeworks/vladimir.varavva_Acid-base/homework_3/js/main.js
@@ -5,7 +5,7 @@
     var nativeSetTimeout = window.setTimeout;
     window.setTimeout = newSetTimeout();
     function newSetTimeout() {
-        var i = 1;
+        var id = 1;
         return function(delay, callback) {
             var args = Array.prototype.slice.call(arguments, 2);
             if (typeof callback === 'function') {
@@ -13,27 +13,27 @@
                     callback.apply(this, args);
                 }, delay);
             }
-            return i++;
+            return id++;
         };
     }
-
+     
     //2
     window.setInterval = newSetInterval();
     function newSetInterval() {
-        var i = 1;
+        var id = 1;
         return function(callback, delay) {
             var args = Array.prototype.slice.call(arguments, 2);
             setTimeout(delay, function loop() {
                 callback.apply(this, args);
                 setTimeout(delay, loop);
             });
-            return i++;
+            return id++;
         };
     }
 
     //3
     function freeze(delay, fnc) {
-        var timeout = true;
+        var timeout;
         return function() {
             var args = arguments;
             timeout = setTimeout(delay, function() {
