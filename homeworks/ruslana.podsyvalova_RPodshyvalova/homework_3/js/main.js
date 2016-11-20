a489784964b83291d2cb8b540e86239a1f5b2b66
RPodshyvalova <pointContacts@gmail.com>
2016-11-20 13:23:48 +0200

    js_homework_#3 add comments

--- homeworks/ruslana.podsyvalova_RPodshyvalova/homework_3/js/main.js
+++ homeworks/ruslana.podsyvalova_RPodshyvalova/homework_3/js/main.js
@@ -1,11 +1,7 @@
 (function() {
-    // 1)
-
     /*
-    function showMessage() {
-        console.log("Hello world!");
-    }
-    */
+        task 1
+     */
     var setTimeoutArchiv = setTimeout;
     setTimeout = function(delay, callback) {
         if (arguments.length === 2 && typeof(callback) === "function"
@@ -17,7 +13,9 @@
         console.log("Hello world! - from task #1");
     });
 
-    // 2)
+    /*
+        task 2
+     */
     setInterval = function(callback, delay) {
         setTimeout(delay, function emulateSetIntervalAct() {
             callback();
@@ -28,12 +26,14 @@
         console.log("Hello world! - from task #2");
     },  2000);
 
-    // 3)
+    /*
+        task 3
+     */
     function fncToDelay(param) {
         console.log("Delayed run : " + param);
     }
-
-    // to do canceling(clearTimeout) - for all frozenFunc('2')...frozenFunc('9'), besides frozenFunc('1')
+    
+    // to do canceling(clearTimeout) - for all frozenFunc("2")...frozenFunc("9"), besides frozenFunc("1")
     function freeze(delay, fnc) {
         var timeout;
         var firstTimerId = 0;
@@ -51,17 +51,19 @@
         }
     }
     var frozenFunc = freeze(1000, fncToDelay);
-    frozenFunc('1');
-    frozenFunc('2');
-    frozenFunc('3');
-    frozenFunc('4');
-    frozenFunc('5');
-    frozenFunc('6');
-    frozenFunc('7');
-    frozenFunc('8');
-    frozenFunc('9');
+    frozenFunc("1");
+    frozenFunc("2");
+    frozenFunc("3");
+    frozenFunc("4");
+    frozenFunc("5");
+    frozenFunc("6");
+    frozenFunc("7");
+    frozenFunc("8");
+    frozenFunc("9");
 
-    // 4)
+    /*
+        task 4
+     */
     function createPipe(originalFunction, arrayOfFilters) {
         return function() {
             function runFilter(strParam, index) {
@@ -73,19 +75,22 @@
             originalFunction.apply(this, new Array(runFilter(arguments[0], arrayOfFilters.length)));
         }
     }
-
+    
     function originalFnc(string) {
         var result = string.replace(/\b[a-z]/g, function() {
             return arguments[0].toUpperCase();
         });
         console.log(result);
     }
+    
     function filterDigits(string) {
         return string.replace(/\d+/g, "");
     }
+   
     function filterSpecial(string) {
         return string.replace(/[!@#$%^&*()+=]/g, "");
     }
+    
     function filterWhiteSpaces(string) {
         return string.replace(/(\s+){1,}/g, " ");
     }
@@ -95,19 +100,17 @@
      you need just to describe a new filter, like  function addDotAtTheEnd(string) for example,
      and push it to array - filters
     */
-    /*
     function addDotAtTheEnd(string) {
         return string + ".";
     }
-    */
-
+    
     var filters = [];
     filters.push(filterDigits);
     filters.push(filterSpecial);
     filters.push(filterWhiteSpaces);
-    //filters.push(addDotAtTheEnd);
+    filters.push(addDotAtTheEnd);
     console.log("Results from task #4: ");
     var pipe = createPipe(originalFnc, filters);
-    pipe('on345l90y    te**x((((t     h$&er@@@e'); // logs 'Only Text Here'
+    pipe("on345l90y    te**x((((t     h$&er@@@e"); // logs "Only Text Here"
     console.log("\n\n");
 })();
