d9718ea2965834707b73816b63363783d5450bfb
Max Musiienko <maksonoff@gmail.com>
2016-11-21 11:11:12 +0200

    Homework_3.1

--- homeworks/Maksym.Musiienko_Sempl/homework_3/js/main.js
+++ homeworks/Maksym.Musiienko_Sempl/homework_3/js/main.js
@@ -3,18 +3,16 @@
     // Task 1
     
     var winSetTimeout = window.setTimeout;
-
     
     window.setTimeout = function (delay, callback) {
-        var args = [];
-            
+        
+        var args = [];            
         args[0] = callback;
         args[1] = delay;
 
         return winSetTimeout.apply(null, args);
+        
     };
-
-
    
     //test
     //setTimeout(3000, function(){console.log("Hello");});
@@ -22,31 +20,39 @@
     // Task 2
 
     function chgSetInterval(callback, delay) {
+        
         setTimeout(delay, function () {
             callback();
             chgSetInterval(callback, delay);
         });
+
     }
 
     window.setInterval = chgSetInterval;
 
     //test
     //setInterval(function(){console.log("Hello");}, 3000);
-
-
    
 
     // Task 3
     function fncToDelay (param) {
+        
         console.log('Delayed run : ' + param);
     }
 
+
     function freeze(delay, fnc) {
+
         var timeout;
+
         return function() {
+
             var args = arguments;
+
             if (timeout) {
+
                 return timeout;
+
             }                          
                 
             timeout = setTimeout(delay, function() {
@@ -55,7 +61,6 @@
                 
         }
     }
-
     
     /*var frozenFunc = freeze(1000, fncToDelay);
     frozenFunc('1');
@@ -73,27 +78,36 @@
     function createPipe(originalFnc, filters) {
   
         return function(result) {
+            
             for (var i = 0; i < filters.length; i++) {
+
                 result = filters[i](result);
+
             }
+           
             originalFnc(result);
+
         }
     }
 
     function originalFnc (string) {
+        
         var arrStr = string.toLowerCase().split(' '),
             result;
   
         for (var i = 0; i < arrStr.length; i++) {
+
             arrStr[i] = arrStr[i].charAt(0).toUpperCase() + arrStr[i].slice(1);
+       
         }
   
         result = arrStr.join(' ');
         console.log(result);
+
     }
 
 
-     function filterDigits (string) {
+    function filterDigits (string) {
         
         var result = string.replace(/\d+/g, '');
 
