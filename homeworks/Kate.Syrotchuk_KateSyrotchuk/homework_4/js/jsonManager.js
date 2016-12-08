b040044f5d51d93405d17b5c16950de94782107f
kate.syrotchuk <katerinasyrotchuk@gmail.com>
2016-12-08 17:04:19 +0200

    added template for work with json

---
+++ homeworks/Kate.Syrotchuk_KateSyrotchuk/homework_4/js/jsonManager.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    function readTextFile(file, callback) {
+        var rawFile = new XMLHttpRequest();
+        rawFile.overrideMimeType("application/json");
+        rawFile.open("GET", file, true);
+        rawFile.onreadystatechange = function() {
+            if (rawFile.readyState === 4 && rawFile.status == "200") {
+                callback(rawFile.responseText);
+            }
+        };
+        rawFile.send(null);
+    }
+
+    readTextFile("files/file.json", function(text){
+        var data = JSON.parse(text);
+        console.log(data);
+    });
+
+});
