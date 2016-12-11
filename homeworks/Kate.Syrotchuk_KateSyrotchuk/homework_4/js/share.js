6846cf3295f2c90355a2c11224d728aee34ce627
kate <katerinasyrotchuk@gmail.com>
2016-12-11 15:33:34 +0200

    fix typo

--- homeworks/Kate.Syrotchuk_KateSyrotchuk/homework_4/js/share.js
+++ homeworks/Kate.Syrotchuk_KateSyrotchuk/homework_4/js/share.js
@@ -3,9 +3,7 @@ document.addEventListener('DOMContentLoaded', function () {
     var vkBtn = document.getElementById('vk');
 
     vkBtn.addEventListener('click', function () {
-        // console.log(VK.Share.button());
         VK.Share.click(0, VK.Share.button());
-
     }, false);
 
 });
