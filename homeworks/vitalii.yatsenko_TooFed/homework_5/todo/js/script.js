645b14f19902b840c40012daf9adc3ea3a73fedd
TooFed <yatsik95@gmail.com>
2016-12-13 17:31:59 +0200

    alpha todo list

--- homeworks/vitalii.yatsenko_TooFed/homework_5/todo/js/script.js
+++ homeworks/vitalii.yatsenko_TooFed/homework_5/todo/js/script.js
@@ -1,36 +1,56 @@
 (function() {
-    
-    $(".table tr").click(function() {
+    $(".table").on('click', 'tr', function() {
         if (this.firstChild.id === 'head-table') {
-            console.log(this);
+            //console.log(this);
         } else if (this.className === 'selected') {
             $(this).removeClass('selected');
-            console.log(this);
+            //console.log(this);
         } else {
             $(this).addClass('selected').siblings().removeClass('selected');
         }
-        console.log(this);
+        //console.log(this);
     });
-    
     $("#add-item").click(function() {
         var taskbody = document.createElement('tr');
         taskbody.innerHTML = ('<th><input type="checkbox" name="items">test</th>');
-        var table = document.querySelector('.table');
+        var table = document.querySelector('tbody');
         table.appendChild(taskbody);
     });
-    
     $("#del-item").click(function() {
         var tr = document.querySelector('.selected');
         tr.remove();
     });
-
-    $("#mark-done").click(function(){
-       // var checked = document.getElementsByAttribute('checked','');
-        var checked = document.getElementsBy('checked');
-       // querySelectorAll("[xlink|href='"+id+"']");
-        
-        console.log(checked);
-
+    $("#mark-done").click(function() {
+        // var checked = document.getElementsByAttribute('checked','');
+        var table = document.querySelector('tbody');
+        var checked = table.querySelectorAll(':checked');
+        for (var i = 0; i < checked.length; i++) {
+            checked[i].parentNode.setAttribute('style', 'text-decoration: line-through');
+        }
+    });
+    $("#mark-not-done").click(function() {
+        // var checked = document.getElementsByAttribute('checked','');
+        var table = document.querySelector('tbody');
+        var checked = table.querySelectorAll(':checked');
+        for (var i = 0; i < checked.length; i++) {
+            checked[i].parentNode.removeAttribute('style');
+        }
+    });
+    $("#mark-all").click(function(){
+        var table = document.querySelector('tbody');
+        //var checked = table.querySelectorAll(':checked');
+        var input = table.querySelectorAll('input');
+        var i=0;
+        if(this.checked === true){
+            for (i ; i < input.length; i++){
+                input[i].setAttribute('checked','true');
+            }
+        }
+        if(this.checked === false){
+            console.log('false');
+            for (i ; i < input.length; i++){
+                input[i].removeAttribute('checked');
+            }
+        }
     })
-
 })();
