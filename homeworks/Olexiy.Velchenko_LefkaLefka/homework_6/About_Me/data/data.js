4a6695c42c7fd45e51ec2ee1d264e2a197e4f8b6
LefkaLefka <lefka.velchenko@gmail.com>
2016-12-19 21:14:54 +0200

    Added new folder for "About me" project

---
+++ homeworks/Olexiy.Velchenko_LefkaLefka/homework_6/About_Me/data/data.js
@@ -0,0 +1,6 @@
+window.appData = {
+    "img" : "https://pp.vk.me/c638830/v638830374/14aa2/8smnqheLn7w.jpg",
+    "name" : "Olexiy Velchenko",
+    "post" : "Software Engineer",
+    "info" : "I am 22 years old. I study system analysis. I am dealing with programming an development of different projects for \"Adruino\". I am also writing poetry and prose.",
+};
---
+++ homeworks/Olexiy.Velchenko_LefkaLefka/homework_6/About_Me/data/dataShareLinks.js
@@ -0,0 +1,6 @@
+window.appShareLinks = {
+    "Vkonakte" : "http://vkontakte.ru/share.php?url=",
+    "Facebook" : "http://www.facebook.com/sharer.php?s=100&p[url]=",
+    "Twitter" : "https://twitter.com/intent/tweet?text=&url=",
+    "GooglePlus" : "https://plus.google.com/share?url="
+};
