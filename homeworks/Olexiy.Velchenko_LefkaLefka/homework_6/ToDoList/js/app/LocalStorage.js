e7d12b6aae06869eec7d3b8969f6f33256cf29f8
LefkaLefka <lefka.velchenko@gmail.com>
2016-12-23 17:13:16 +0200

    Added functions for save/load data in local storage

---
+++ homeworks/Olexiy.Velchenko_LefkaLefka/homework_6/ToDoList/js/app/LocalStorage.js
@@ -0,0 +1,26 @@
+function LocalStorage() {
+    this.$el = $("[data-content-list]");
+}
+LocalStorage.prototype = {
+    saveData : function() {
+        var $all = this.$el.children(),
+            allTaskObj = {};
+        $all.each(function(i, elem) {
+            var taskObj = {
+                text : $(elem).find($("[data-task-text]")).text(),
+                status : $(elem).find($("[data-checkbox-done]")).prop("checked")
+            };
+            allTaskObj[i] = taskObj;
+        });
+        // allTaskObj = allTaskObj.slice(0, allTaskObj.length - 1);
+        // console.log(JSON.stringify(allTaskObj));
+        localStorage.setItem("toDoList", JSON.stringify(allTaskObj));
+    },
+    loadData : function() {
+        var allTaskObj = JSON.parse(localStorage.getItem("toDoList"));
+
+        for(var key in allTaskObj) {
+            new ListItem(allTaskObj[key].text, allTaskObj[key].status);
+        }
+    }
+};
