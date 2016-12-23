556c6bdb22e02358dc89a117d05e68f811e03364
LefkaLefka <lefka.velchenko@gmail.com>
2016-12-23 17:06:49 +0200

    Added double click listener

--- homeworks/Olexiy.Velchenko_LefkaLefka/homework_6/ToDoList/js/app/GlobalEventListener.js
+++ homeworks/Olexiy.Velchenko_LefkaLefka/homework_6/ToDoList/js/app/GlobalEventListener.js
@@ -1,21 +1,23 @@
 function GlobalEventListener() {
-    // this.$el = $("[data-delete-button]");
-
     var functional = new GlobalFunctional();
+    var filters = new Filters();
 
     this.listenCheckboxChange($("[data-drop-down]"), functional.dropDown);
-    this.listenKeyPressed($("[data-new-task]"), functional.addNewTask);
+    this.listenKeyPressed($("[data-new-task]"), functional.addNewTask.bind(functional));
 
-    this.listenClickEvent($("[data-filter-all]"), functional.showAll);
-    this.listenClickEvent($("[data-filter-active]"), functional.showActive);
-    this.listenClickEvent($("[data-filter-Completed]"), functional.showCompleted);
-    this.listenClickEvent($("[data-remove-completed]"), functional.deleteCompleted);
+    this.listenClickEvent($("[data-filter-all]"), filters.showAll.bind(filters));
+    this.listenClickEvent($("[data-filter-active]"), filters.showActive.bind(filters));
+    this.listenClickEvent($("[data-filter-completed]"), filters.showCompleted.bind(filters));
+
+    this.listenClickEvent($("[data-remove-completed]"), functional.deleteCompleted.bind(functional));
 }
-
 
 GlobalEventListener.prototype = {
     listenClickEvent : function(element, callback) {
         $(element).on("click", callback);
+    },
+    listenDblClickEvent : function(element, callback) {
+        $(element).on("dblclick", callback);
     },
     listenCheckboxChange : function(element, callback) {
         $(element).on("change", callback);
