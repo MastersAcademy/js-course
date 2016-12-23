11fa7407ba2b9a67e29939b2bbc63acaafcb8c02
LefkaLefka <lefka.velchenko@gmail.com>
2016-12-23 17:11:50 +0200

    Added events for load/save data in local storage.

--- homeworks/Olexiy.Velchenko_LefkaLefka/homework_6/ToDoList/js/main.js
+++ homeworks/Olexiy.Velchenko_LefkaLefka/homework_6/ToDoList/js/main.js
@@ -1,1 +1,10 @@
-new GlobalEventListener();
+$(document).ready(function() {
+    new GlobalEventListener();
+    var localStorage = new LocalStorage();
+    localStorage.loadData();
+    GlobalFunctional.prototype.countToDoLeft();
+
+    window.onbeforeunload = function() {
+        localStorage.saveData();
+    };
+});
