9e685f121d39584bffc918780bbf068827945620
LefkaLefka <lefka.velchenko@gmail.com>
2016-12-25 14:38:33 +0200

    Rewritten using "window"

--- homeworks/Olexiy.Velchenko_LefkaLefka/homework_6/ToDoList/js/main.js
+++ homeworks/Olexiy.Velchenko_LefkaLefka/homework_6/ToDoList/js/main.js
@@ -1,10 +1,27 @@
-$(document).ready(function() {
-    new GlobalEventListener();
-    var localStorage = new LocalStorage();
-    localStorage.loadData();
-    GlobalFunctional.prototype.countToDoLeft();
+(function() {
+    if(window.toDoList) {
+        window.toDoList.globalFunctional = new GlobalFunctional();
+        window.toDoList.filters = new Filters();
+        window.toDoList.globalEventListener = new GlobalEventListener();
+        window.toDoList.localStorage = new LocalStorage();
+    }
+    else {
+        window.toDoList = {
+            globalFunctional : new GlobalFunctional(),
+            filters : new Filters(),
+            globalEventListener : new GlobalEventListener(),
+            localStorage : new LocalStorage()
+        };
+    }
 
-    window.onbeforeunload = function() {
-        localStorage.saveData();
-    };
-});
+    window.toDoList.globalEventListener.init();
+
+    $(document).ready(function() {
+        window.toDoList.localStorage.loadData();
+        window.toDoList.globalFunctional.countToDoLeft();
+
+        window.onbeforeunload = function() {
+            window.toDoList.localStorage.saveData();
+        };
+    });
+})();
