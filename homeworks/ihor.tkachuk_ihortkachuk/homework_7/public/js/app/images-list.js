b6849c235ba55b786025205754fb3c519a916286
Ihor Tkachuk <ihortkachuk@yandex.ua>
2016-12-25 16:50:08 +0200

    homework 7

---
+++ homeworks/ihor.tkachuk_ihortkachuk/homework_7/public/js/app/images-list.js
@@ -0,0 +1,50 @@
+function ImagesList() {
+
+    this.$el = $('[data-items]');
+
+    this.imageTpl = this.$el.find('[data-image-item]').get(0).outerHTML;
+
+    this.$el.empty();
+
+    //this.images = window.appData;
+    this.getData();
+
+}
+
+ImagesList.prototype = {
+
+    getData: function () {
+
+        $.get('/images')
+
+            .done(function (response) {
+
+                this.images = response;
+
+                this.buildList();
+
+            }.bind(this))
+
+            .fail(function (error) {
+
+                alert(error.statusText);
+
+            })
+
+    },
+
+    buildList: function () {
+
+        this.images.forEach(this.createImageItem.bind(this));
+
+    },
+
+    createImageItem: function (imageData) {
+
+        var newItem = new ImageItem(this.imageTpl, imageData);
+
+        this.$el.append(newItem.$el);
+
+    }
+
+};
