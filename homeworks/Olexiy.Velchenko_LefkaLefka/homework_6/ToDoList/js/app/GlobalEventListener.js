820a09ddc128e1b6fb42aa8e69baeeab221ec6a4
LefkaLefka <lefka.velchenko@gmail.com>
2016-12-25 14:37:55 +0200

    Rewritten using "window". Added new function to init

--- homeworks/Olexiy.Velchenko_LefkaLefka/homework_6/ToDoList/js/app/GlobalEventListener.js
+++ homeworks/Olexiy.Velchenko_LefkaLefka/homework_6/ToDoList/js/app/GlobalEventListener.js
@@ -1,18 +1,24 @@
 function GlobalEventListener() {
-    var functional = new GlobalFunctional();
-    var filters = new Filters();
-
-    this.listenCheckboxChange($("[data-drop-down]"), functional.dropDown);
-    this.listenKeyPressed($("[data-new-task]"), functional.addNewTask.bind(functional));
-
-    this.listenClickEvent($("[data-filter-all]"), filters.showAll.bind(filters));
-    this.listenClickEvent($("[data-filter-active]"), filters.showActive.bind(filters));
-    this.listenClickEvent($("[data-filter-completed]"), filters.showCompleted.bind(filters));
-
-    this.listenClickEvent($("[data-remove-completed]"), functional.deleteCompleted.bind(functional));
+    // this.init();
 }
 
 GlobalEventListener.prototype = {
+    init : function() {
+        this.listenCheckboxChange($("[data-drop-down]"),
+            window.toDoList.globalFunctional.dropDown);
+        this.listenKeyPressed($("[data-new-task]"),
+            window.toDoList.globalFunctional.addNewTask.bind(window.toDoList.globalFunctional));
+
+        this.listenClickEvent($("[data-filter-all]"),
+            window.toDoList.filters.showAll.bind(window.toDoList.filters));
+        this.listenClickEvent($("[data-filter-active]"),
+            window.toDoList.filters.showActive.bind(window.toDoList.filters));
+        this.listenClickEvent($("[data-filter-completed]"),
+            window.toDoList.filters.showCompleted.bind(window.toDoList.filters));
+
+        this.listenClickEvent($("[data-remove-completed]"),
+            window.toDoList.globalFunctional.deleteCompleted.bind(window.toDoList.globalFunctional));
+    },
     listenClickEvent : function(element, callback) {
         $(element).on("click", callback);
     },
