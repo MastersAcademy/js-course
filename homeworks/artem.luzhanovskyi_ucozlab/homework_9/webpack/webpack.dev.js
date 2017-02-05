ec9981ecb79206b28eb16358b2b1baf95177e713
ucozlab <la-3111@mail.ru>
2017-02-05 12:57:05 +0200

    base build

---
+++ homeworks/artem.luzhanovskyi_ucozlab/homework_9/webpack/webpack.dev.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const HtmlWebpack = require('html-webpack-plugin');
+const path = require('path');
+const webpack = require('webpack');
+const ChunkWebpack = webpack.optimize.CommonsChunkPlugin;
+
+const rootDir = path.resolve(__dirname, '..');
+
+module.exports = {
+    // debug: true,
+    devServer: {
+        contentBase: path.resolve(rootDir, 'dist'),
+        port: 9000
+    },
+    devtool: 'source-map',
+    entry: {
+        app: [ path.resolve(rootDir, 'src', 'app', 'scripts', 'main.ts') ],
+        vendor: [ path.resolve(rootDir, 'src', 'vendor', 'main.ts') ]
+    },
+    module: {
+        loaders: [
+            { loader: 'raw-loader', test: /\.(css|html)$/ },
+            { exclude: /node_modules/, loader: 'ts-loader', test: /\.ts$/ }
+        ]
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(rootDir, 'dist')
+    },
+    plugins: [
+        new ChunkWebpack({
+            filename: 'vendor.bundle.js',
+            minChunks: Infinity,
+            name: 'vendor'
+        }),
+        new HtmlWebpack({
+            filename: 'index.html',
+            inject: 'body',
+            template: path.resolve(rootDir, 'src', 'app', 'index.html')
+        })
+    ],
+    resolve: {
+        extensions: [ '.js', '.ts' ]
+    }
+};
