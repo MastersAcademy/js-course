43877cca943a6c4f1e498924a16f54c42e290c39
Acid-base <bart133@gmail.com>
2017-02-06 21:34:01 +0200

    hw10

---
+++ homeworks/vladimir.varavva_Acid-base/homework_10/app/shared/mock-heroes.ts
@@ -0,0 +1,27 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    {
+        id: 1,
+        name: 'Deadpool',
+        skills: 'Regenerative healing factor',
+        img: 'http://i.imgur.com/R076K0l.jpg'
+    },
+    {
+        id: 2,
+        name: 'Daredevil',
+        skills: 'Superhuman senses',
+        img: 'https://i.annihil.us/u/prod/marvel/i/mg/3/00/55084b4f107e0/landscape_xlarge.jpg'
+    },
+    {
+        id: 3,
+        name: 'Doctor Strange',
+        skills: 'Mastery of magic',
+        img: 'https://i.annihil.us/u/prod/marvel/i/mg/f/d0/568acf89abd5b.jpg'
+    },
+    {
+        id: 4,
+        name: 'Ghost Rider',
+    },
+
+];
