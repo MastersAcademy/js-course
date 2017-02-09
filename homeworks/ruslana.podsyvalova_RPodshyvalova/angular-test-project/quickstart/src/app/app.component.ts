bb3beb413416686863a5de3b85f5eb4f48f13eab
RPodshyvalova <pointcontacts@gmail.com>
2017-02-09 20:50:24 +0200

    homework_10

--- homeworks/ruslana.podsyvalova_RPodshyvalova/angular-test-project/quickstart/src/app/app.component.ts
+++ homeworks/ruslana.podsyvalova_RPodshyvalova/angular-test-project/quickstart/src/app/app.component.ts
@@ -1,23 +1,19 @@
 import { Component } from '@angular/core';
-
-export class Hero {
-    id: number;
-    name: string;
-    photo: string;
-}
+import { Hero } from './app.hero';
 
 const PATH_TO_IMAGES = 'src/images/';
+const ANTIHEROES: string[]  = ["Spawn", "Deadpool", "Wolverine", "Punisher", "Ghost Rider", "Rorschach", "Blade", "Venom", "Deathstroke", "John Constantine"  ]; 
 const HEROES: Hero[] = [
-    { id: 11, name: 'Mr. Nice', photo: PATH_TO_IMAGES  +"Nice.jpg" },
-    { id: 12, name: 'Narco', photo: PATH_TO_IMAGES  +"Narco.jpg" },
-    { id: 13, name: 'Bombasto', photo: PATH_TO_IMAGES  +"Bombasto.jpg" },
-    { id: 14, name: 'Celeritas', photo: PATH_TO_IMAGES +"Celeritas.jpg" },
-    { id: 15, name: 'Magneta', photo: PATH_TO_IMAGES  +"Magneta.jpg" },
-    { id: 16, name: 'RubberMan', photo: PATH_TO_IMAGES +"RubberMan.jpg" },
-    { id: 17, name: 'Dynama', photo: PATH_TO_IMAGES +"Dynama.jpg" },
-    { id: 18, name: 'Dr IQ', photo: PATH_TO_IMAGES + "DrIQ.jpg" },
-    { id: 19, name: 'Magma', photo: PATH_TO_IMAGES + "Magma.jpg" },
-    { id: 20, name: 'Tornado', photo: PATH_TO_IMAGES + "Tornado.jpg" }
+    { id: 11, name: 'Mr. Nice', photo: PATH_TO_IMAGES  + "Nice.jpg", antihero: ANTIHEROES[0] },
+    { id: 12, name: 'Narco', photo: PATH_TO_IMAGES  + "Narco.jpg", antihero: ANTIHEROES[1] },
+    { id: 13, name: 'Bombasto', photo: PATH_TO_IMAGES  + "Bombasto.jpg", antihero: ANTIHEROES[2] },
+    { id: 14, name: 'Celeritas', photo: PATH_TO_IMAGES + "Celeritas.jpg", antihero: ANTIHEROES[3] },
+    { id: 15, name: 'Magneta', photo: PATH_TO_IMAGES  + "Magneta.jpg", antihero: ANTIHEROES[4] },
+    { id: 16, name: 'RubberMan', photo: PATH_TO_IMAGES + "RubberMan.jpg", antihero: ANTIHEROES[5] },
+    { id: 17, name: 'Dynama', photo: PATH_TO_IMAGES + "Dynama.jpg", antihero: ANTIHEROES[6] },
+    { id: 18, name: 'Dr IQ', photo: PATH_TO_IMAGES + "DrIQ.jpg", antihero: ANTIHEROES[7] },
+    { id: 19, name: 'Magma', photo: PATH_TO_IMAGES + "Magma.jpg", antihero: ANTIHEROES[8] },
+    { id: 20, name: 'Tornado', photo: PATH_TO_IMAGES + "Tornado.jpg", antihero: ANTIHEROES[9] }
 ];
 
 @Component({
@@ -29,9 +25,21 @@ const HEROES: Hero[] = [
 export class AppComponent {
     title = 'Tour of Heroes';
     heroes = HEROES;
+    antiheroes = ANTIHEROES;
     selectedHero: Hero;
-
-    onSelect(hero: Hero): void {
+    editedHero: Hero;
+        
+    // when user click on an item of hero -
+    // show hero details block and hide hero edit block
+    onShowDetails(hero: Hero): void {
         this.selectedHero = hero;
+        this.editedHero = null;    
+    }
+    // when user click on an edit button -
+    // show hero edit block and hide hiro details block
+    onEdit(hero: Hero, event: any): void {
+        event.stopPropagation();
+        this.editedHero = hero;
+        this.selectedHero = null;    
     }
 }
--- homeworks/ruslana.podsyvalova_RPodshyvalova/angular-test-project/quickstart/src/app/app.module.ts
+++ homeworks/ruslana.podsyvalova_RPodshyvalova/angular-test-project/quickstart/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { HeroDetailComponent } from './app.hero-detail.component';
+import { HeroEditComponent } from './app.hero-edit.component';
 
 @NgModule({
     imports: [ BrowserModule, FormsModule ],
-    declarations: [ AppComponent ],
+    declarations: [ AppComponent,  HeroDetailComponent, HeroEditComponent],
     bootstrap: [ AppComponent ]
 })
 
