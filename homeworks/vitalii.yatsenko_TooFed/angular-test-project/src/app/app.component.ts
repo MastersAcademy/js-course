05d94c97df0f80d41614ffcd1de5a516ef80fa92
TooFed <yatsik95@gmail.com>
2017-02-10 18:33:07 +0200

    Homework 10

--- homeworks/vitalii.yatsenko_TooFed/angular-test-project/src/app/app.component.ts
+++ homeworks/vitalii.yatsenko_TooFed/angular-test-project/src/app/app.component.ts
@@ -1,17 +1,9 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
 
-const HEROES: Hero[] = [
-	{ id: 11, name: 'Flash', image: 'https://s-media-cache-ak0.pinimg.com/564x/85/74/d0/8574d06d28de8be754aa736adbd954b3.jpg' },
-	{ id: 12, name: 'Arrow' , image: 'http://ep.yimg.com/ay/stylinonline/green-arrow-face-t-shirt-13.jpg'},
-	{ id: 13, name: 'Superman', image: 'https://s-media-cache-ak0.pinimg.com/564x/b4/80/e3/b480e3024ae72b1eedf09c96285c551b.jpg' },
-	{ id: 14, name: 'Batman', image: 'http://images.shirts.com/products/28908/14-14-57496/batman-cartoon-face-alt-1.jpg' },
-	{ id: 15, name: 'Green lantern', image: 'http://img13.deviantart.net/7ba3/i/2014/028/b/6/green_lantern_drawing_by_nealadams-d4ltl7s.jpg' },
-	{ id: 16, name: 'Wonder Woman', image: 'http://www.clipartkid.com/images/70/wonder-woman-emblem-clipart-best-lcfVfF-clipart.jpeg' },
-	{ id: 17, name: 'Martian Manhunter', image: 'https://s-media-cache-ak0.pinimg.com/236x/90/79/a1/9079a1595cbf057f08b539bf23ccb5df.jpg' },
-	{ id: 18, name: 'Hawkgirl', image: 'http://img08.deviantart.net/94ff/i/2013/113/f/e/hawkgirl_by_yamelme-d62pk0e.jpg' },
-	{ id: 19, name: 'Aquaman', image: 'https://s-media-cache-ak0.pinimg.com/564x/94/ee/1d/94ee1d2147723f7827da241fdb7ce8be.jpg' },
-	{ id: 20, name: 'Cyborg', image: 'https://imgs-tuts-dragoart-386112.c.cdn77.org/how-to-draw-cyborg-easy_1_000000021098_5.png' }
-];
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
+
 
 @Component({
 	selector: 'my-app',
@@ -26,17 +18,8 @@ const HEROES: Hero[] = [
   				<img src="{{hero.image}}" class="heroes-face">
 			</li>
 		</ul>
-	    <div *ngIf="selectedHero">
-    		<h2>{{selectedHero.name}} details!</h2>
-    		<div>
-    			<label>id: </label>{{selectedHero.id}}
-    		</div>
-    		<div>
-        		<label>name: </label>
-        		<input [(ngModel)]="selectedHero.name" placeholder="name"/>
-        		<label>hero image: </label>{{selectedHero.image}}
-      		</div>
-    	</div>	
+		<my-hero-detail [hero]="selectedHero"></my-hero-detail>
+		<my-hero-edit [hero]="selectedHero" [hidden]="hidden"></my-hero-edit>
   		`,
   	styles: [`
 	  .selected {
@@ -91,21 +74,28 @@ const HEROES: Hero[] = [
 		height: 35px;
 		float: right;
 	 }
-	`]
+	`],
+	providers: [HeroService]
 })
 
-export class AppComponent  {
+export class AppComponent implements OnInit {
 	title = 'Tour of Heroes';
-	heroes = HEROES;
+	heroes : Hero[];
 	selectedHero: Hero;
+	hidden : boolean = true;
+
+	constructor(private heroService: HeroService) { }
+
 	onSelect(hero: Hero): void {
+			this.hidden = false;
     	this.selectedHero = hero;
     }
-}
 
-export class Hero {
-	id: number;
-	name: string;
-	image: string;
-}
+	getHeroes(): void {
+		this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+	}
 
+    ngOnInit(): void {
+    	this.getHeroes();
+    }
+}
---
+++ homeworks/vitalii.yatsenko_TooFed/angular-test-project/src/app/mock-heroes.ts
@@ -0,0 +1,25 @@
+import { Hero } from './hero';
+
+
+export const HEROES: Hero[] = [
+	{ id: 11, name: 'Flash', skill: 'fast',
+	image: 'https://s-media-cache-ak0.pinimg.com/564x/85/74/d0/8574d06d28de8be754aa736adbd954b3.jpg' },
+	{ id: 12, name: 'Arrow' , skill: 'green:)' ,
+	image: 'http://ep.yimg.com/ay/stylinonline/green-arrow-face-t-shirt-13.jpg'},
+	{ id: 13, name: 'Superman', skill: 'impregnable',
+	image: 'https://s-media-cache-ak0.pinimg.com/564x/b4/80/e3/b480e3024ae72b1eedf09c96285c551b.jpg' },
+	{ id: 14, name: 'Batman', skill: 'smth',
+	image: 'http://images.shirts.com/products/28908/14-14-57496/batman-cartoon-face-alt-1.jpg' },
+	{ id: 15, name: 'Green lantern', skill: 'smth',
+	image: 'http://img13.deviantart.net/7ba3/i/2014/028/b/6/green_lantern_drawing_by_nealadams-d4ltl7s.jpg' },
+	{ id: 16, name: 'Wonder Woman', skill: 'amazonas',
+	image: 'http://www.clipartkid.com/images/70/wonder-woman-emblem-clipart-best-lcfVfF-clipart.jpeg' },
+	{ id: 17, name: 'Martian Manhunter', skill: 'smth',
+	image: 'https://s-media-cache-ak0.pinimg.com/236x/90/79/a1/9079a1595cbf057f08b539bf23ccb5df.jpg' },
+	{ id: 18, name: 'Hawkgirl', skill: 'smth',
+	image: 'http://img08.deviantart.net/94ff/i/2013/113/f/e/hawkgirl_by_yamelme-d62pk0e.jpg' },
+	{ id: 19, name: 'Aquaman', skill: 'smth',
+	image: 'https://s-media-cache-ak0.pinimg.com/564x/94/ee/1d/94ee1d2147723f7827da241fdb7ce8be.jpg' },
+	{ id: 20, name: 'Cyborg', skill: 'smth',
+	image: 'https://imgs-tuts-dragoart-386112.c.cdn77.org/how-to-draw-cyborg-easy_1_000000021098_5.png' }
+];
